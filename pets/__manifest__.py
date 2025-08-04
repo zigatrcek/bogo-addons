c867c7aeb25f5c559dd420334bd8800d825a1333
Ziga Trcek <ziga.trc@gmail.com>
2025-08-04 22:53:18 +0000

    Add pets module

--- pets/__manifest__.py
+++ pets/__manifest__.py
@@ -1,16 +1,18 @@
 {
-    'name': 'Pets',
-    'summary': 'Manage your pets\' needs.',
-    'category': 'Services',
-    'website': 'https://bogosort.xyz',
-    'depends': ['account', 'contacts'],
-    'data': [
+    "name": "Pets",
+    "summary": "Manage your pets' needs.",
+    "category": "Services",
+    "author": "Bogosort",
+    "website": "https://bogosort.xyz",
+    "depends": ["account", "contacts"],
+    "data": [
+        "views/pets_pet_views.xml",
+        "views/menu.xml",
+        "security/ir.model.access.csv",
     ],
-    'demo': [
-    ],
-    'installable': True,
-    'application': True,
-    'assets': {
-    },
-    'license': 'Other proprietary',
+    "demo": [],
+    "installable": True,
+    "application": True,
+    "assets": {},
+    "license": "Other proprietary",
 }
--- pets/models/pets_pet.py
+++ pets/models/pets_pet.py
@@ -3,12 +3,13 @@ from odoo import api, fields, models
 
 class PetsPet(models.Model):
     _name = 'pets.pet'
-    
-    name = fields.Char()
+    _inherit = ["image.mixin"]
+    _description = "Pet"
+
+    name = fields.Char(required=True)
     official_name = fields.Char()
     date_of_birth = fields.Date()
-    pet_type = fields.Selection(selection=[
-        ('cat', 'Cat'),
-        ('dog', 'Dog')
-    ])
-    
+    pet_type = fields.Selection(
+        selection=[("cat", "Cat"), ("dog", "Dog")], required=True
+    )
+    sex = fields.Selection([("male", "Male"), ("female", "Female")], required=True)
